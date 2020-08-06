e30cf8e9d0a9bd6dc2b896445c657e6fe92b40bc
Ashwini Sandip Sabale <53634901+ashwinisabale@users.noreply.github.com>
2020-08-06 12:46:54 +0530

    Add files via upload

---
+++ templates/navbar.php
@@ -0,0 +1,30 @@
+ <!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My Profile</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/sign-in/">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Farm Vegetables</label>
+      <ul>
+		<li><a class="active" href="#">Home</a></li>
+		<li><a href="AUserProfile.php">UserProfile</a></li>
+		<li><a href="#">New Order </a></li>
+		<li><a href="OrderHist.php">Order History</a></li>
+		<li><a href="AdminProduct.php">Product Update</a></li>
+		<li><a href="ALogOut.php">Logout</a></li>
+	   </ul>
+	</nav>
